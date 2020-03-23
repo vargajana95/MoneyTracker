8f82bde2b8b62993fcef90acf6602be74b889c03
vargajana95 <vargajana95@gmail.com>
2020-03-23 16:38:57 +0100

    Inject TransactionsInteractor to TransactionsPresenter

--- app/src/main/java/vargajana95/moneytracker/ui/transactions/TransactionsPresenter.kt
+++ app/src/main/java/vargajana95/moneytracker/ui/transactions/TransactionsPresenter.kt
@@ -1,8 +1,10 @@
 package vargajana95.moneytracker.ui.transactions
 
+import vargajana95.moneytracker.interactor.TransactionInteractor
 import vargajana95.moneytracker.ui.Presenter
+import javax.inject.Inject
 
-class TransactionsPresenter : Presenter<SummaryScreen>() {
+class TransactionsPresenter @Inject constructor(val transactionInteractor: TransactionInteractor): Presenter<SummaryScreen>() {
 
     fun show() {
         screen?.show("Hello")
